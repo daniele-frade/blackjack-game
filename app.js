80729bb0470f6ef4c82f82a2a27909ebf9d1b962
daniele-frade <63023863+danielefrade@users.noreply.github.com>
2021-05-11 15:22:34 -0400

    starting with the logics

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // Steps: 
 
+// store the cards
+let dealerCards = []
+let playerCards = []
+
+// generate a deck of cards
+
+// start the game
+
+// return random card from the deck
+
+// calculate points for each player
+
+// buttons hit and stand
+
